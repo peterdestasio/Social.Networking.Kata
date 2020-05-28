91236def27e8c142bdf5d889ee185bfd727678c0
Peter <peterdestasio@gmail.com>
2020-05-28 00:07:40 -0400

    add Newtonsoft.Json dll

---
+++ Social.Networking.Kata.BusinessService.Test/CommandParserUnitTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Social.Networking.Kata.BusinessService.Interfaces;
+using Social.Networking.Kata.BusinessService.Services;
+
+namespace Social.Networking.Kata.BusinessService.UnitTest
+{
+    [TestClass]
+    public class CommandParserUnitTest
+    {
+        private ICommandParser _commandParser;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _commandParser = new CommandParser();
+        }
+
+
+        [TestMethod]
+        public void Test_Follow_Command()
+        {
+            var input = "Peter follows Mary Poppins";
+            var result = _commandParser.parse(input);
+            string[] expected = { "Peter", "follows", "Mary Poppins" };
+            Assert.AreEqual(result[0], expected[0]);
+            Assert.AreEqual(result[1], expected[1]);
+            Assert.AreEqual(result[2], expected[2]);
+        }
+
+        [TestMethod]
+        public void Test_Post_To_Wall_Command()
+        {
+            var input = "Peter -> hey this is My wall";
+            var result = _commandParser.parse(input);
+            string[] expected = { "Peter", "->", "hey this is My wall" };
+            Assert.AreEqual(result[0], expected[0]);
+            Assert.AreEqual(result[1], expected[1]);
+            Assert.AreEqual(result[2], expected[2]);
+        }
+
+        [TestMethod]
+        public void Test_View_Wall_Command()
+        {
+            var input = "Peter";
+            var result = _commandParser.parse(input);
+            string[] expected = { "Peter", ""};
+            Assert.AreEqual(result[0], expected[0]);
+        }
+
+
+        [TestMethod]
+        public void Test_View_Wall_By_Id_Command()
+        {
+            var input = "Peter wall";
+            var result = _commandParser.parse(input);
+            string[] expected = { "Peter", "wall" };
+            Assert.AreEqual(result[0], expected[0]);
+            Assert.AreEqual(result[1], expected[1]);
+        }
+    }
+}
---
+++ Social.Networking.Kata.BusinessService.Test/MessageServiceUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Social.Networking.Kata.BusinessService.Interfaces;
+using Social.Networking.Kata.BusinessService.Services;
+using Social.Networking.Kata.DAL.Interfaces;
+using Social.Networking.Kata.DAL.Model;
+using System;
+using System.Collections.Generic;
+
+namespace Social.Networking.Kata.BusinessService.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IMessageService _messageService;
+
+        private readonly Mock<IMessageRepository> _messageRepository = new Mock<IMessageRepository>();
+        private readonly Mock<ITimeFormatter> _timeFormatter = new Mock<ITimeFormatter>();
+        private readonly Mock<IFollowRepository> _followRepository = new Mock<IFollowRepository>();
+        [TestInitialize]
+        public void Initialize()
+        {
+            _messageService = new MessageService(_messageRepository.Object, _timeFormatter.Object, _followRepository.Object);
+        }
+
+
+        [TestMethod]
+        public void Test_View_Private_Wall()
+        {
+            var wallList = new List<WallMessage>();
+            wallList.Add(new WallMessage { UserId = "Mary", Message = "hey Peter", Time = DateTime.Now });
+            _messageRepository.Setup(m => m.getPrivateWall(It.IsAny<string>())).Returns(wallList);
+            _timeFormatter.Setup(m => m.elapsedMinutes(It.IsAny<DateTime>())).Returns("10");
+            var actual = _messageService.viewPrivateWall("Mary", true);
+            var expected = "Mary - hey Peter (10 minutes ago)\r\n";
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Test_View_Private_Wall_No_Show_Id()
+        {
+            var wallList = new List<WallMessage>();
+            wallList.Add(new WallMessage { UserId = "Mary", Message = "hey Peter", Time = DateTime.Now });
+            _messageRepository.Setup(m => m.getPrivateWall(It.IsAny<string>())).Returns(wallList);
+            _timeFormatter.Setup(m => m.elapsedMinutes(It.IsAny<DateTime>())).Returns("10");
+            var actual = _messageService.viewPrivateWall("Mary", false);
+            var expected = "hey Peter (10 minutes ago)\r\n";
+            Assert.AreEqual(actual, expected);
+        }
+
+    }
+}
